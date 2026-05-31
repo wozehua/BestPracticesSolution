356ae1d66e9fc68be850c6b6de640b3c04049c83
wozehua <273362054@qq.com>
2026-05-31 16:59:40 +0800

    feat: 结构体代替类性能测试

---
+++ 使用结构体代替类Benchmarks/Benchmarks.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.VSDiagnostics;
+using System;
+using System.Security.Cryptography;
+
+namespace 使用结构体代替类Benchmarks
+{
+    // For more information on the VS BenchmarkDotNet Diagnosers see https://learn.microsoft.com/visualstudio/profiling/profiling-with-benchmark-dotnet
+    [CPUUsageDiagnoser]
+    public class Benchmarks
+    {
+        private SHA256 sha256 = SHA256.Create();
+        private byte[]? data;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new byte[10000];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256()
+        {
+            return sha256.ComputeHash(data??new byte[10000]);
+        }
+    }
+}
---
+++ 使用结构体代替类Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace 使用结构体代替类Benchmarks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
